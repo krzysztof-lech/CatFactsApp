48d509da0ff25433e8caf917e3546ea364f2421c
Krzysztof Lech <krzysztof.lech02@gmail.com>
2026-04-29 16:59:12 +0200

    feat: implement CatFactService

---
+++ src/CatFactsApp/Services/CatFactService.cs
@@ -0,0 +1,31 @@
+using System.Net.Http.Json;
+
+namespace CatFactsApp.Services;
+
+public class CatFactService : ICatFactService
+{
+    private readonly HttpClient _httpClient;
+
+
+
+    public CatFactService(HttpClient httpClient)
+    {
+        _httpClient = httpClient;
+    }
+
+
+    public async Task<CatFactDto> FetchAndSaveFactAsync()
+    {
+
+            var result = await _httpClient.GetFromJsonAsync<CatFactDto>("https://catfact.ninja/fact");
+            
+            if (result == null) 
+            {
+                throw new Exception("API did not return any data.");
+            }
+            
+            await File.AppendAllTextAsync("CatFacts.txt", $"Fact: {result.Fact}{Environment.NewLine}Length: {result.Length}{Environment.NewLine}");
+
+            return result;
+    }
+}
