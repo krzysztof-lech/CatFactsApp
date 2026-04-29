f71538f40a11f463314d6acad7c6a7ad7472ed9d
Krzysztof Lech <krzysztof.lech02@gmail.com>
2026-04-29 21:54:17 +0200

    test: implement unit tests for CatFactService using Moq

---
+++ tests/CatFactsApp.Tests/CatFactServiceTests.cs
@@ -0,0 +1,77 @@
+using CatFactsApp.Services;
+using CatFactsApp.Models;
+using System.Net;
+using Moq;
+using Moq.Protected;
+using Microsoft.Extensions.Configuration;
+using Xunit;
+
+namespace CatFactsApp.Tests;
+
+
+public class CatFactServiceTests
+{
+    [Fact]
+    public async Task FetchAndSaveFactAsync_ReturnsFact_WhenApiIsSuccessful()
+    {
+       // --- ARRANGE ---
+        
+        var handlerMock = new Mock<HttpMessageHandler>();
+        var response = new HttpResponseMessage
+        {
+            StatusCode = HttpStatusCode.OK,
+            Content = new StringContent("{\"fact\":\"Cats are liquid.\",\"length\":15}"),
+        };
+        response.Content.Headers.ContentType = new System.Net.Http.Headers.MediaTypeHeaderValue("application/json");
+
+        handlerMock
+            .Protected()
+           .Setup<Task<HttpResponseMessage>>(
+              "SendAsync",
+              ItExpr.IsAny<HttpRequestMessage>(),
+              ItExpr.IsAny<CancellationToken>()
+           )
+           .ReturnsAsync(response);
+
+        var httpClient = new HttpClient(handlerMock.Object);
+        httpClient.BaseAddress = new Uri("https://api.test/"); 
+
+        var configMock = new Mock<IConfiguration>();
+
+        configMock.Setup(c => c["FileSettings:Path"]).Returns("test_facts.txt");
+
+        var service = new CatFactService(httpClient, configMock.Object);
+
+        // --- ACT ---
+        var result = await service.FetchAndSaveFactAsync();
+
+        // --- ASSERT ---
+        Assert.NotNull(result);
+        Assert.Equal("Cats are liquid.", result.Fact);
+
+
+    }
+
+    [Fact]
+    public async Task FetchAndSaveFactAsync_ThrowsException_WhenApiReturnsNull()
+    {
+        // --- ARRANGE ---
+        var handlerMock = new Mock<HttpMessageHandler>();
+        var response = new HttpResponseMessage
+        {
+            StatusCode = HttpStatusCode.OK,
+            Content = new StringContent("null"), 
+        };
+        response.Content.Headers.ContentType = new System.Net.Http.Headers.MediaTypeHeaderValue("application/json");
+
+        handlerMock.Protected()
+        .Setup<Task<HttpResponseMessage>>("SendAsync", ItExpr.IsAny<HttpRequestMessage>(), ItExpr.IsAny<CancellationToken>())
+        .ReturnsAsync(response);
+
+        var service = new CatFactService(new HttpClient(handlerMock.Object) { BaseAddress = new Uri("https://api.test/") }, new Mock<IConfiguration>().Object);
+
+        // --- ACT & ASSERT ---
+        var exception = await Assert.ThrowsAsync<Exception>(() => service.FetchAndSaveFactAsync());
+        Assert.Equal("API did not return any data.", exception.Message);
+    }
+}
